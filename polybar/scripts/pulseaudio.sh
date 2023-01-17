a9090c95f22db42bb90a0d9e5c335e33acf0c3d4
vvh413 <nnenekit@gmail.com>
2023-01-17 21:44:37 +0300

    initial

---
+++ polybar/scripts/pulseaudio.sh
@@ -0,0 +1,24 @@
+#!/bin/sh
+
+TYPE=sink
+PAMIXER_ARG=
+
+if [[ $MIC = "1" ]]; then
+    TYPE=source
+    PAMIXER_ARG=--default-source
+fi
+
+listen() {
+    pamixer $PAMIXER_ARG --get-volume-human
+    pactl subscribe | while read -r event; do
+        if echo "$event" | grep -q $TYPE || echo "$event" | grep -q "server"; then
+            pamixer $PAMIXER_ARG --get-volume-human
+        fi
+    done
+}
+
+if [[ $1 == "" ]]; then
+    listen
+else
+    pamixer $PAMIXER_ARG $@
+fi
