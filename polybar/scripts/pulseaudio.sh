4699759bb10494a289adc851c0fc9dcf107ba650
vvh413 <nnenekit@gmail.com>
2023-09-05 03:40:20 +0300

    clear shit + polybar nmcli script + bg3 floating mode

--- polybar/scripts/pulseaudio.sh
+++ polybar/scripts/pulseaudio.sh
@@ -4,21 +4,21 @@ TYPE=sink
 PAMIXER_ARG=
 
 if [[ $MIC = "1" ]]; then
-    TYPE=source
-    PAMIXER_ARG=--default-source
+	TYPE=source
+	PAMIXER_ARG=--default-source
 fi
 
 listen() {
-    pamixer $PAMIXER_ARG --get-volume-human
-    pactl subscribe | while read -r event; do
-        if echo "$event" | grep -q $TYPE || echo "$event" | grep -q "server"; then
-            pamixer $PAMIXER_ARG --get-volume-human
-        fi
-    done
+	pamixer $PAMIXER_ARG --get-volume-human
+	pactl subscribe | while read -r event; do
+		if echo "$event" | grep -q $TYPE || echo "$event" | grep -q "server"; then
+			pamixer $PAMIXER_ARG --get-volume-human
+		fi
+	done
 }
 
 if [[ $1 == "" ]]; then
-    listen
+	listen
 else
-    pamixer $PAMIXER_ARG $@
+	pamixer $PAMIXER_ARG $@
 fi
---
+++ polybar/scripts/sources.sh
@@ -0,0 +1,30 @@
+#!/bin/sh
+
+count() {
+	pcount=$(pactl list source-outputs short | wc -l)
+	if [[ $pcount -ne "0" ]]; then
+		echo $pcount
+	else
+		echo ""
+	fi
+}
+
+monitor() {
+	count
+	pactl subscribe | while read -r event; do
+		if echo "$event" | grep -q "source" || echo "$event" | grep -q "server"; then
+			count
+		fi
+	done
+}
+
+rofi_kill() {
+	SOURCES=$(pactl -f json list source-outputs)
+	IDX=$(echo $SOURCES | jq -r '.[] | .properties."application.name" + ": " + .properties."media.name"' | rofi -dmenu -i -format i -p "$1")
+	[[ $IDX == "" ]] || echo $SOURCES | jq ".[$IDX].properties.\"application.process.id\" | tonumber" | xargs kill
+}
+
+case "$1" in
+kill) rofi_kill ;;
+*) monitor ;;
+esac
