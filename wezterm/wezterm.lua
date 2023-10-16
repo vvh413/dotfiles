dfe7774cdc0ed3afde253a1b772735d0514663df
vvh413 <nnenekit@gmail.com>
2023-10-16 15:36:59 +0300

    open url keybind ctrl+lmb

--- wezterm/wezterm.lua
+++ wezterm/wezterm.lua
@@ -34,5 +34,14 @@ config.max_fps = 120
 
 config.warn_about_missing_glyphs = false
 
+config.mouse_bindings = {
+	-- Ctrl-click will open the link under the mouse cursor
+	{
+		event = { Up = { streak = 1, button = "Left" } },
+		mods = "CTRL",
+		action = wezterm.action.OpenLinkAtMouseCursor,
+	},
+}
+
 -- and finally, return the configuration to wezterm
 return config
