cb74cb5627cd8478a3d415bf5b5a01cbf38eebe2
vvh <ns@ywa.red>
2024-08-01 01:13:33 +0300

    herbst: Added menu for minimized client unminization

---
+++ config/x11/herbstluftwm/minimized_menu.sh
@@ -0,0 +1,22 @@
+#!/bin/env sh
+
+minimized() {
+  herbstclient foreach CLIENT clients. \
+    sprintf MINATT "%c.minimized" CLIENT \
+    sprintf WINID "%{%c.winid}" CLIENT \
+    and , compare MINATT = true , echo WINID
+}
+
+titles() {
+  a=( $@ )
+  for client in ${a[@]}; do
+    herbstclient get_attr "clients.$client.title"
+  done
+}
+
+clients=( $(minimized) )
+sel=$( titles "${clients[@]}" | rofi -dmenu -i -format i -p "Minimized clients")
+
+if [[ -n "$sel" ]]; then
+  herbstclient bring ${clients[$sel]}
+fi
