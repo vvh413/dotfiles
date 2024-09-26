7921ba697dc6655b92a7998c7babf52e03056853
vvh <ns@ywa.red>
2024-09-26 15:52:35 +0300

    chore(macro): use bspc onshot rule with receptacle instead of semi-manual preselects

--- local/macro/private_to_left.sh
+++ local/macro/private_to_left.sh
@@ -1,8 +1,7 @@
 #!/bin/sh
 
-bspc node -f west
-bspc node -c
-sleep 0.2
-bspc node -p west
-bspc node -o 0.282
+if [ $(bspc query -d focused -N | wc -l) -eq 1 ]; then
+	bspc node -p west -o 0.282 -i
+fi
+bspc rule -a firefox:Navigator -o node=@/1
 firefox --private-window &
