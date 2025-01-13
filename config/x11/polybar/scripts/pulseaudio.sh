391d53b76058f72b066663cf25afaa96811cd713
vvh <ns@ywa.red>
2025-01-13 16:19:37 +0300

    feat(polybar): Send mic status to keyboard via VIA

--- config/x11/polybar/scripts/pulseaudio.sh
+++ config/x11/polybar/scripts/pulseaudio.sh
@@ -3,22 +3,37 @@
 TYPE=sink
 PAMIXER_ARG=
 
-if [[ $MIC = "1" ]]; then
-  TYPE=source
-  PAMIXER_ARG=--default-source
+if [[ "$MIC" == "1" ]]; then
+	TYPE=source
+	PAMIXER_ARG=--default-source
 fi
 
+send_status() {
+	if [[ "$MIC" != "1" ]]; then
+		return
+	fi
+
+	status=$([[ "$1" == "muted" ]] && echo 0 || echo 1)
+	vvh-via-cli set-mic-state $status
+}
+
+get_volume() {
+	volume=$(pamixer $PAMIXER_ARG --get-volume-human)
+	send_status "$volume"
+	echo $volume
+}
+
 listen() {
-  pamixer $PAMIXER_ARG --get-volume-human
-  pactl subscribe | while read -r event; do
-    if echo "$event" | grep -q $TYPE || echo "$event" | grep -q "server"; then
-      pamixer $PAMIXER_ARG --get-volume-human
-    fi
-  done
+	get_volume
+	pactl subscribe | while read -r event; do
+		if echo "$event" | grep -q $TYPE || echo "$event" | grep -q "server"; then
+			get_volume
+		fi
+	done
 }
 
 if [[ $1 == "" ]]; then
-  listen
+	listen
 else
-  pamixer $PAMIXER_ARG $@
+	pamixer $PAMIXER_ARG $@
 fi
