aa599f32a35ad51c920f60f082528f38d363490a
vvh <ns@ywa.red>
2025-02-01 21:27:52 +0300

    chore(fish): Added async-prompt + minor pure prompt customizations

--- config/common/fish/config.fish
+++ config/common/fish/config.fish
@@ -6,6 +6,7 @@ zoxide init fish | source
 s739 generate fish | source
 
 set -g fish_greeting
+set -g async_prompt_functions _pure_prompt_git
 
 # Added by LM Studio CLI (lms)
 set -gx PATH $PATH /home/vvh413/.cache/lm-studio/bin
