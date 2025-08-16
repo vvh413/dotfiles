73d9b885f9d42af634647eb878616ed790875130
vvh <ns@ywa.red>
2025-08-16 16:01:01 +0300

    fix: fixed chaotic pgp keys + formatting

--- chaotic-yay.sh
+++ chaotic-yay.sh
@@ -4,23 +4,23 @@ set -e
 
 # yay
 if ! type "yay" >/dev/null; then
-  sudo pacman -Sy --noconfirm git
-  git clone https://aur.archlinux.org/yay.git
-  cd yay
-  makepkg -si --noconfirm
-  cd ..
-  rm -rf yay/
+	sudo pacman -Sy --noconfirm git
+	git clone https://aur.archlinux.org/yay.git
+	cd yay
+	makepkg -si --noconfirm
+	cd ..
+	rm -rf yay/
 fi
 
 # chaotic
 if ! grep -Fq "chaotic-aur" /etc/pacman.conf; then
-  sudo pacman-key --recv-key FBA220DFC880C036 --keyserver keyserver.ubuntu.com
-  sudo pacman-key --lsign-key FBA220DFC880C036
-  sudo pacman -U --noconfirm 'https://cdn-mirror.chaotic.cx/chaotic-aur/chaotic-keyring.pkg.tar.zst' \
-    'https://cdn-mirror.chaotic.cx/chaotic-aur/chaotic-mirrorlist.pkg.tar.zst'
-  echo "
+	sudo pacman-key --recv-key 3056513887B78AEB --keyserver keyserver.ubuntu.com
+	sudo pacman-key --lsign-key 3056513887B78AEB
+	sudo pacman -U --noconfirm 'https://cdn-mirror.chaotic.cx/chaotic-aur/chaotic-keyring.pkg.tar.zst'
+	sudo pacman -U --noconfirm 'https://cdn-mirror.chaotic.cx/chaotic-aur/chaotic-mirrorlist.pkg.tar.zst'
+	echo "
 [chaotic-aur]
 Include = /etc/pacman.d/chaotic-mirrorlist
 " | sudo tee -a /etc/pacman.conf
-  yay -Syy
+	yay -Syy
 fi
