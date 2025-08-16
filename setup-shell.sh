bf74744b4290040f8ea1bba9ef251da2c38ce353
vvh <ns@ywa.red>
2025-08-16 16:33:30 +0300

    fix: fixed fisher installation

--- setup-shell.sh
+++ setup-shell.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
 
 chsh -s $(which fish)
-curl -sL https://raw.githubusercontent.com/jorgebucaran/fisher/main/functions/fisher.fish | source && fisher install jorgebucaran/fisher
+fish -c 'curl -sL https://raw.githubusercontent.com/jorgebucaran/fisher/main/functions/fisher.fish | source && fisher install jorgebucaran/fisher'
