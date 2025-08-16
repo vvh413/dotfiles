8444779d83cbc39cf8e3fe30ba97ec54b898a004
vvh <ns@ywa.red>
2025-08-16 15:48:32 +0300

    fix(bspwm): fixed init

--- local/macro/init_bspwm.sh
+++ local/macro/init_bspwm.sh
@@ -4,8 +4,8 @@ bspc wm -l ~/.config/bspwm/states/default.json
 bspc rule -a org.wezfurlong.wezterm:org.wezfurlong.wezterm -o node=@^1:^1:/1
 bspc rule -a firefox:Navigator -o node=@^1:^1:/2
 bspc rule -a firefox:Navigator -o node=@^2:^1:/1
-bspc rule -a TelegramDesktop:telegram-desktop -o node=@^2:^1:/2 state=tiled
+bspc rule -a TelegramDesktop:Telegram -o node=@^2:^1:/2 state=tiled
 
 wezterm &
 firefox &
-telegram-desktop &
+Telegram &
